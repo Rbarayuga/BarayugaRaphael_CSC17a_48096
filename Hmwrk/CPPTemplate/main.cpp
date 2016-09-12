8a5bb6cd5c5c66b8bb9361f66c0d1a428b365867
rbarayuga <raphael.barayuga@gmail.com>
2016-09-12 13:04:38 -0700

    Template

---
+++ Hmwrk/CPPTemplate/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Raphael M.B. Barayuga
+ * Created on September 12, 2016, 1:01 PM
+ * Purpose: Simple template for all your programs
+ */
+
+//system Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution begins here!
+int main(int argc, char** argv) {
+     //Declare Variables
+    
+     //Input Data
+    
+    //Process -> Map the input data to the output
+    
+    //Display/Output solution here
+    return 0;
+}
+
