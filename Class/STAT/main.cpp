bd2478cd46ddb8188da14dad36ddb85f89009119
rcc <rcc>
2016-09-19 13:41:06 -0700

    Signed-off-by: rcc <rcc>

---
+++ Class/STAT/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Raphael M.B. Barayuga
+ *
+ * Created on September 19, 2016, 1:37 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+   //mean median moade
+    
+    //n E l=x XsubL /N
+    
+    //sort X
+    
+    //Median formula N/2-1  + N/2 % 2 
+    
+    //input array return array and 3.4.5.6
+    
+    //Hmodes
+    return 0;
+}
+
