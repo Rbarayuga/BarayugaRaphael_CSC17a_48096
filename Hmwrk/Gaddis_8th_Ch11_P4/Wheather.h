3805ef4b0b908a35e5f038720fc6bf7fde5b59e4
rcc <rcc>
2016-10-06 13:25:12 -0700

    Signed-off-by: rcc <rcc>

---
+++ Hmwrk/Gaddis_8th_Ch11_P4/Wheather.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Wheather.h
+ * Author: rcc
+ *
+ * Created on October 6, 2016, 1:18 PM
+ */
+
+#ifndef WHEATHER_H
+#define WHEATHER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* WHEATHER_H */
+
